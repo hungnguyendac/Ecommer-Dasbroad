b1cd6e64e3c1b001b46166cae8d6488a97764168
NDHung <hungnguyendac6@gmail.com>
2024-12-23 15:55:38 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/Ecommer-Dasbroad/", plugins: [react()] });
+export default defineConfig({ base: "/", plugins: [react()] });
